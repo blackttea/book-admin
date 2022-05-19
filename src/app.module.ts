a7a417b032522c6d5a6d88701df745e1b27310c2
wsj <shaojin.wang@vixtel.com>
2022-05-19 18:47:01 +0800

    4. RBAC

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UserController } from './logical/user/user.controller';
 import { AuthModule } from './logical/auth/auth.module';
 import { UserModule } from './logical/user/user.module';
+import { CommodityModule } from './logical/commodity/commodity.module';
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [AuthModule, UserModule, CommodityModule],
   controllers: [AppController, UserController],
   providers: [AppService],
 })
